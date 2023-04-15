ca162af15d1f1a6bdd0d49b5e5475c751680519c
Ravie403 <info@tkm.blue>
2023-04-15 20:15:47 +0900

    wip

--- react-app/src/components/BorrowView.tsx
+++ react-app/src/components/BorrowView.tsx
@@ -6,11 +6,11 @@ import { useMockLendingMarketBorrow, usePrepareMockLendingMarketBorrow } from ".
 
 const BorrowView = () => {
   const { config, error } = usePrepareMockLendingMarketBorrow({
-    address: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+    address: '0x6D544390Eb535d61e196c87d6B9c80dCD8628Acd',
     args: [
-      '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+      '0x5FbDB2315678afecb367f032d93F642f64180aa3',
       BigNumber.from('42'),
-      '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+      '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
       '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
     ],
   })
