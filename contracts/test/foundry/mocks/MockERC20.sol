c6e53f8fc89e18d67cddb178a2de04ed9084c982
massun-onibakuchi <etherdao0x00@gmail.com>
2023-04-15 10:54:51 +0900

    add mock

--- contracts/test/foundry/mocks/MockERC20.sol
+++ contracts/test/foundry/mocks/MockERC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20Mock is ERC20 {
+contract MockERC20 is ERC20 {
     constructor() ERC20("Test", "TEST") {}
 
     function mint(address account, uint256 amount) public {
